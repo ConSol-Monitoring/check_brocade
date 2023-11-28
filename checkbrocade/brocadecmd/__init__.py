f4b29651db350f805ede761df7a8c4cc2a9caf38
hias <hias@consol.de>
2023-11-28 12:45:28 +0100

    v0.1a2 mode about,hardwarehealth,interfacehealth

--- checkbrocade/brocadecmd/__init__.py
+++ checkbrocade/brocadecmd/__init__.py
@@ -12,14 +12,3 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-__version__ = "0.1"
-
-class CheckBrocadeException(Exception):
-    pass
-
-class CheckBrocadeTimeout(BaseException):
-    pass
-
-class CheckBrocadeConnnectException(Exception):
-    pass
