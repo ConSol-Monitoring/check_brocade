698d88be628b06b79e100d98a9e300b30c7ce7c1
hias <hias@consol.de>
2025-04-03 09:30:50 +0200

    about: add version endpoint

--- checkbrocade/brocadecmd/about.py
+++ checkbrocade/brocadecmd/about.py
@@ -19,7 +19,7 @@
 import logging
 from monplugin import Check,Status
 from ..tools import cli
-from ..tools.helper import severity
+from ..tools.helper import severity, compare_versions, convert_keys
 from ..tools.connect import broadcomAPI
 from pprint import pprint as pp
 
@@ -38,27 +38,36 @@ def run():
     parser.set_description(description)
     args = parser.get_args()
 
-    # Setup module logging 
+    # Setup module logging
     logger = logging.getLogger(__name__)
     logger.disabled=True
     if args.verbose:
         for log_name, log_obj in logging.Logger.manager.loggerDict.items():
             log_obj.disabled = False
             logging.getLogger(log_name).setLevel(severity(args.verbose))
-            
+
     check = Check()
     try:
         plugin(check)
     except Exception as e:
         logger.error(f"{e}")
         check.exit(Status.UNKNOWN, f"{e}")
-    
+
 def plugin(check):
     base_url = f"https://{args.host}:{args.port}"
     api = broadcomAPI(logger, base_url, args.username, args.password, args.sessionfile)
-    response_data = api.make_request("GET", "rest/running/brocade-chassis/chassis")
-    chassis = response_data['chassis']
-    check.add_message(Status.OK, f"{chassis['manufacturer']} {chassis['product-name']} FOS {api.version(True)} S/N {chassis['vendor-serial-number']}")
+    response = api.make_request("GET", "rest/running/brocade-chassis/chassis")
+    chass = convert_keys(response)
+    chassis = chass.chassis
+    if compare_versions("9.2.0", api.version(True)):
+        response = api.make_request("GET", "rest/running/brocade-chassis/version")
+        ns = convert_keys(response)
+        version = ns.version.fabric_os
+    else:
+        logger.info("FOS < 9.2.0 use version from API response")
+        version = api.version(True)
+
+    check.add_message(Status.OK, f"{chassis.manufacturer} {chassis.product_name} FOS {version} S/N {chassis.vendor_serial_number}")
     (code, message) = check.check_messages(separator="\n")
     check.exit(code=code,message=message)
 
